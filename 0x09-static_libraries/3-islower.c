992c10d820a71044bf86f8062ce87ec64d663b8a
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2023-10-07 20:54:31 +0000

    Task-0: Modified the 3-islower.c file to have the correct function

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase character
+ * @c: character to be checked
+ *
+ * Return: 1 if character is lowercase, 0 if not.
+ */
+
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
