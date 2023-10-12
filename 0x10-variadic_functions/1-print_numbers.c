ca2f67b3a1df7077e22024eb619feda781829d36
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2023-10-12 11:34:16 +0000

    fixed visible betty errors

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,19 +9,19 @@
  * Return: 0
  */
 
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)	
 {
 	unsigned int i;
-	va_list = ap;
+	va_list ap;
 
 	va_start(ap, n);
 
 	if (separator == NULL)
 		separator = "";
 
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(ap,int));
+		printf("%d", va_arg(ap, int));
 		if (i < n - 1)
 			printf("%s", separator);
 	}
