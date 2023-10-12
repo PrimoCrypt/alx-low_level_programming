664cd2245b4c2bf4804c6c86579dba65075c3f8d
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2023-10-12 11:19:01 +0000

    Task-0: Added logic to reset sum to 0 always

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,14 +12,14 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum;
+	int sum = 0;
 	unsigned int i;
 	va_list ap;
 
 	va_start(ap, n);
 
 	if (n == 0)
-		return 0;
+		return (0);
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
 
